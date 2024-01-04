5973e657f8f5006f532247110ef55baa319b6469
Walidomar2 <walidomar291@gmail.com>
2024-01-04 08:02:32 +0200

    nothing

--- binery to decimal/main.c
+++ binery to decimal/main.c
@@ -3,21 +3,21 @@
 
 int main()
 {
-    int dec=0 , bin , rem ,num ,base=1;
+    int dec = 0, bin, rem, num, base = 1;
 
     printf("Enter the binary number : ");
     scanf("%d", &bin);
 
-    num=bin;
-    while(bin!=0)
+    num = bin;
+    while (bin != 0)
     {
-        rem=bin%10;
-        dec=dec+rem*base;
-        bin=bin/10;
-        base=base*2;
-    }
-    printf("the equivalant of binary %d is %d ", num ,dec);
+        rem = bin % 10;
 
+        dec = dec + rem * base;
+        bin = bin / 10;
+        base = base * 2;
+    }
+    printf("the equivalant of binary %d is %d ", num, dec);
 
     return 0;
 }
